d2e0e52860c8fe6c27d8190bc01d62fcd889ccaa
Enmanuel Minaya <enmanuelminaya@gmail.com>
2021-12-04 18:33:00 -0400

    kata tdd finish

--- doorkata.js
+++ doorkata.js
@@ -1,9 +1,76 @@
+class Door {
 
-//https://github.com/EnmanuelMinaya/doorkata.git
+    #key;
 
-const door = 'open';
+    static OpenAndUnlocked() {
+        let d = new Door('', true, false, true, false);
+        return d;
+    }
 
-const closesDoor = (door) => door == 'open' ? 'closed' : console.log('error');
+    static OpenAndlocked() {
+        let d = new Door('', true, false, false, true);
+        return d;
+    }
 
+    static ClosedAndUnlocked() {
+        let d = new Door('', false, true, true, false);
+        return d;
+    }
 
-module.exports = { closesDoor }
+    static ClosedAndLocked(key) {
+        let d = new Door(key, false, true, false, true);
+        return d;
+    }
+
+    get key() { return this.key; }
+    set key(value) { this.#key = value; }
+
+    close() {
+        if (this.isClosed)
+            throw new Error(`Invalid door action`);
+        else if (this.isOpen && this.isLocked) {
+            return Door.ClosedAndLocked();
+        }
+        else if (this.isOpen && this.isUnlocked) {
+            return Door.ClosedAndUnlocked();
+        }
+    }
+
+    open() {
+        if (this.isOpen)
+            throw new Error(`Invalid door action`);
+        else if (this.isClosed && this.isLocked) {
+            throw new Error(`Invalid door action`);
+        }
+        else if (this.isClosed && this.isUnlocked) {
+            return Door.OpenAndUnlocked();
+        }
+    }
+
+    lock() {
+        if (this.isLocked)
+            throw new Error(`invalid door action`);
+        if (this.isClosed && this.isUnlocked) {
+            return Door.ClosedAndLocked();
+        }
+    }
+
+    unlock(key) {
+        if (this.isUnlocked)
+            throw new Error(`invalid door action`);
+        if (key != this.#key)
+            throw new Error(`wrong key`);
+        if (this.isClosed && this.isLocked && key == this.#key) {
+            return Door.ClosedAndUnlocked();
+        }
+    }
+
+    constructor(key, isOpen, isClosed, isUnlocked, isLocked) {
+        this.#key = key;
+        this.isOpen = isOpen;
+        this.isClosed = isClosed;
+        this.isUnlocked = isUnlocked;
+        this.isLocked = isLocked;
+    }
+}
+module.exports = { Door }
--- doorkata.test.js
+++ doorkata.test.js
@@ -1,6 +1,88 @@
 
-const { closesDoor } = require('./doorkata.js');
+const { Door } = require('./doorkata.js');
 
-test('takes door open returns door close', () => {
-    expect(closesDoor('open')).toBe('closed');
-});
+test('returns true to open door being open', () => {
+    let testDoor = Door.OpenAndUnlocked('1234');
+    expect(testDoor.isOpen).toBe(true)
+})
+
+test('returns false to open door being close', () => {
+    let testDoor = Door.OpenAndUnlocked('1234');
+    expect(testDoor.isClosed).toBe(false)
+})
+
+test('returns true to unlocked door being unlocked', () => {
+    let testDoor = Door.OpenAndUnlocked('1234');
+    expect(testDoor.isUnlocked).toBe(true)
+})
+
+test('returns false to unlocked door being locked', () => {
+    let testDoor = Door.OpenAndUnlocked('1234');
+    expect(testDoor.isLocked).toBe(false)
+})
+
+test('returns false to unlocked door being locked', () => {
+    let testDoor = Door.OpenAndUnlocked('1234');
+    expect(testDoor.isLocked).toBe(false)
+})
+
+test('returns true to closed door being closed', () => {
+    let testDoor = Door.ClosedAndUnlocked('1234');
+    expect(testDoor.isClosed).toBe(true)
+})
+
+test('returns true to locked door being locked', () => {
+    let testDoor = Door.ClosedAndLocked('1234');
+    expect(testDoor.isClosed).toBe(true)
+})
+
+test('closing door returns new close door if door is open ', () => {
+    let t = (Door.OpenAndUnlocked('1234')).close();
+    expect(t.isClosed).toBe(true)
+})
+
+test('should throw and error if door es closed', () => {
+    let t = (Door.ClosedAndUnlocked());
+    expect(() => t.close()).toThrow(`Invalid door action`)
+})
+
+test('should throw an error if door es closed', () => {
+    let t = (Door.ClosedAndUnlocked());
+    expect(() => t.close()).toThrow(`Invalid door action`)
+})
+
+test('opening door should return new open door', () => {
+    let t = (Door.ClosedAndUnlocked().open());
+    expect(t.isOpen).toBe(true);
+})
+
+test('should throw an error if door is already open', () => {
+    let t = Door.OpenAndUnlocked();
+    expect(() => t.open()).toThrow(`Invalid door action`)
+})
+
+test('should return locked door', () => {
+    let t = Door.ClosedAndUnlocked().lock();
+    expect(t.isLocked).toBe(true)
+})
+
+test('should throw an error if door is already locked', () => {
+    let t = Door.ClosedAndLocked();
+    expect(() => t.lock()).toThrow(`invalid door action`)
+})
+
+
+test('should return unlocked door', () => {
+    let t = Door.ClosedAndLocked('1234').unlock('1234');
+    expect(t.isUnlocked).toBe(true)
+})
+
+test('should throw error wrong key', () => {
+    let t = Door.ClosedAndLocked('1234');
+    expect(() => t.unlock('5678')).toThrow(`wrong key`)
+})
+
+test('should throw an error if door is already unlocked', () => {
+    let t = Door.ClosedAndUnlocked();
+    expect(() => t.unlock()).toThrow(`invalid door action`)
+})
